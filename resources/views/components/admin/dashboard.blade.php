c5d7d050f381acbc2a55781d55f80e6b80ad1cf8
Henrique-Ferreira <joaof6418@gmail.com>
2024-04-29 19:20:36 -0300

    fix: save

--- resources/views/components/admin/dashboard.blade.php
+++ resources/views/components/admin/dashboard.blade.php
@@ -1,17 +1,43 @@
 <style>
-    .min-card {}
+    .min-card {
+        height: 200px;
+    }
 
     .max-card {}
 </style>
 
-<div class="w-100 gap-2 ">
-    <h3 class="bg-white shadow-sm px-5 py-3">
-        <br> Dashboard
-        <br>
-    </h3>
-    <div class="rounded shadow-sm w-25 h-25" style="margin-left: 40px;">Total Imóveis
-        <br>
-        48
+@php
+$dashboardDatasList = [
+["title" => "Total de Imóveis", "value" =>"230"],
+["title" => "Imóveis disponíveis", "value" =>"98"],
+["title" => "Renda mensal", "value" =>"R$544.000,00"],
+["title" => "Despesa mensal", "value" => "R$103.000,00"],
+];
+@endphp
+
+<div class="w-100 d-flex flex-column gap-3 shadow-sm">
+    <div class="bg-white shadow-sm-b px-5 py-4 fs-3">
+        Dashboard
+    </div>
+    <div class="d-flex gap-3 text-dark opacity-75 mx-3">
+        @foreach($dashboardDatasList as $data)
+        <div class="bg-white rounded shadow-sm w-100 p-3 ">
+            <span class="mb-2 d-block">{{$data["title"]}}</span>
+            <span class="fw-semibold fs-4 ">{{$data["value"]}}</span>
+        </div>
+        @endForeach
     </div>
 
+    <div class="d-flex gap-1 text-dark opacity-75 mx-3">
+        <div class="bg-white rounded shadow-sm w-100 p-3 ">
+            <span>Histórico de pagamentos</span>
+        </div>
+
+        <div>
+
+        </div>
+        @foreach($dashboardDatasList as $data)
+
+        @endForeach
+    </div>
 </div>
--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -8,10 +8,10 @@ $navList = [
 ];
 @endphp
 
-<div class="d-flex flex-column gap-3 col-2 vh-100 p-2 text-dark opacity-75 bg-white shadow ">
+<div class="d-flex flex-column gap-3 col-2 vh-100 p-2 text-dark opacity-75 bg-white shadow-sm">
     <img class="img-fluid mx-auto" src="https://martinslealimoveis.com.br/imagens/site_modelo/20230315141349565.png" alt="Logo da Imobiliária" style="width: 110px;">
 
-    <div class="border border-bottom border-1"></div>
+    <!-- <div class="border border-bottom border-1"></div> -->
 
     @foreach($navList as $navData)
     <a href="{{$navData['href']}}" class="btn btn-outline-primary border-0 text-start px-4 fw-normal">
