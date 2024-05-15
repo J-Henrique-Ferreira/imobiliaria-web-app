2391a703926b57d096f77e9f89fbac66dc17a6bb
J-Henrique-Ferreira <joaof6418@gmail.com>
2024-05-15 07:34:54 -0300

    chore: create migrations cities, districts, categories, business, users

---
+++ database/migrations/2024_05_14_192048_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * user->role
+     * adm-> adms em geral
+     * master -> maxima liberdade no sistema
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", length: 255)->nullable(false)->unique();
+            $table->string("email", length: 255)->nullable(false)->unique();
+            $table->string("phone_number", length: 20);
+            $table->string("role", length: 45)->default("adm");
+            $table->string("password", length: 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
